ecac314376771fb53359cbd96881722309c84f93
joaogabiazon <jbiazonferreira@gmail.com>
2024-11-13 08:13:38 -0300

    Ajuste na rota

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -7,6 +7,6 @@ router.post('/registration', UserController.registration);
 router.post('/addcoins', UserController.addCoin);
 router.post('/adduid', UserController.addUidUser);
 router.post('/validateUser', UserController.validateUser);
-router.post('/getusercoins', UserController.getUserData);
+router.post('/getuserdata', UserController.getUserData);
 
 export default router;
